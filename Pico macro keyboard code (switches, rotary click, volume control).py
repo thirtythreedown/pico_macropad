09877debdfd17b2d588e31ea614771c5b05522d6
thirtythreedown <32502009+thirtythreedown@users.noreply.github.com>
2023-07-17 20:18:58 -0500

    Update Pico macro keyboard code (switches, rotary click, volume control).py

--- Pico macro keyboard code (switches, rotary click, volume control).py
+++ Pico macro keyboard code (switches, rotary click, volume control).py
@@ -5,24 +5,51 @@ https://www.hackster.io/tyronwakeford/pico-macro-keyboard-and-volume-control-994
 https://lastminuteengineers.com/rotary-encoder-arduino-tutorial/
 """
 
-import board
-import usb_hid
-import time
-import digitalio
-from digitalio import DigitalInOut, Direction
-from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.mouse import Mouse
-from adafruit_hid.keycode import Keycode
+
+import board #importing board module
+import time #importing time module
+import digitalio #importing digitalio module
+import usb_hid #importing USB hid module for keyboard behavior
+from adafruit_hid.keycode import Keycode #importing Keycode module
+from adafruit_hid.keyboard import Keyboard #importing Keyboard module
+from adafruit_hid.mouse import Mouse #importing Mouse module
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
+#import analogio #importing analogio module
 
 led = digitalio.DigitalInOut(board.LED)
-led.direction = Direction.OUTPUT
+led.direction = digitalio.Direction.OUTPUT
 led.value = True
 
-kbd = Keyboard(usb_hid.devices)
-cc = ConsumerControl(usb_hid.devices)
+#This is where we list the pins for the key switches
+btn1_pin = board.GP8
+btn2_pin = board.GP9
+btn3_pin = board.GP10
+btn4_pin = board.GP11
+btn5_pin = board.GP12
 
+#This is where we activate switches and set pull downs/directions for each pin
+btn1 = digitalio.DigitalInOut(btn1_pin)
+btn1.direction = digitalio.Direction.INPUT
+btn1.pull = digitalio.Pull.DOWN
+
+btn2 = digitalio.DigitalInOut(btn2_pin)
+btn2.direction = digitalio.Direction.INPUT
+btn2.pull = digitalio.Pull.DOWN
+
+btn3 = digitalio.DigitalInOut(btn3_pin)
+btn3.direction = digitalio.Direction.INPUT
+btn3.pull = digitalio.Pull.DOWN
+
+btn4 = digitalio.DigitalInOut(btn4_pin)
+btn4.direction = digitalio.Direction.INPUT
+btn4.pull = digitalio.Pull.DOWN
+
+btn5 = digitalio.DigitalInOut(btn5_pin)
+btn5.direction = digitalio.Direction.INPUT
+btn5.pull = digitalio.Pull.DOWN
+
+#This is where we degine the pins for the rotary encoder
 CLK_PIN = board.GP4
 DT_PIN = board.GP3
 SW_PIN = board.GP2
@@ -31,23 +58,24 @@ sw_last = None
 #totalMode = 3
 currentMode = 2
 
+#This is where we create variables to interact with usb_hid.devices
 cc = ConsumerControl(usb_hid.devices)
 mouse = Mouse(usb_hid.devices)
 keyboard = Keyboard(usb_hid.devices)
 
+#This is where we create variables to listen to the rotary encoder's direction
 clk = digitalio.DigitalInOut(CLK_PIN)
 clk.direction = digitalio.Direction.INPUT
 
 dt = digitalio.DigitalInOut(DT_PIN)
 dt.direction = digitalio.Direction.INPUT
 
+#This is where we create variables to listen to the rotary encoder's button press
 sw = digitalio.DigitalInOut(SW_PIN)
 sw.direction = digitalio.Direction.INPUT
 sw.pull = digitalio.Pull.UP
 
-#def millis():
-#    return time.monotonic() * 1000
-
+#These are the functions for bringing volume up, down, and muting the sound
 def ccw():
     print("CCW")
 
@@ -62,11 +90,14 @@ def cw():
 def mute():
     print("Mute")
     cc.send(ConsumerControlCode.MUTE)
-    
 
-while True:
-#    print("The clk.value is" + str(clk.value))
-#    print("The dt.value is" + str(dt.value))
+
+#This is where we define what keyboard stands for
+keyboard = Keyboard(usb_hid.devices)
+
+#This is where the fun begins!
+while True: #setting up infinite loop
+    
     clkState = clk.value
     if(clk_last != clkState):
         if(dt.value != clkState):
@@ -81,4 +112,24 @@ while True:
         mute() #sending out code for 1
         time.sleep(0.2) #sleep for a spell
     sw_last = sw.value
+    
+    #This is where we tie the switch presses to keycodes from the Keycode module
+    if btn1.value: #if button 1 pressed
+        keyboard.send(Keycode.WINDOWS, Keycode.SHIFT, Keycode.LEFT_ARROW) #sending out code for 1
+        time.sleep(0.2) #sleep for a spell
+
+    if btn2.value: #if button 2 pressed
+        keyboard.send(Keycode.WINDOWS, Keycode.SHIFT, Keycode.RIGHT_ARROW) #sending out code for 1
+        time.sleep(0.2) #sleep for a spell
         
+    if btn3.value: #if button 3 pressed
+        keyboard.send(Keycode.WINDOWS, Keycode.D) #sending out code for 1
+        time.sleep(0.2) #sleep for a spell
+        
+    if btn4.value: #if button 4 pressed
+        keyboard.send(Keycode.CONTROL, Keycode.S) #sending out code for 1
+        time.sleep(0.2) #sleep for a spell
+
+    if btn5.value: #if button 4 pressed
+        keyboard.send(Keycode.WINDOWS, Keycode.L) #sending out code for 1
+        time.sleep(0.2) #sleep for a spell
